37a8d7ea36fdd972c92890a79e3636ae57cd24c6
carxpresspanabo <carxpress.panabo@gmail.com>
2025-09-08 20:00:22 +0800

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  // ...other config properties from your Firebase project settings
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
